fcf20f3d420852a4fc80055a0cd4a4212c4f315b
skorovdanil <62069661+skorovdanil@users.noreply.github.com>
2023-11-03 10:10:50 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp;
+
+tg.expend();
+
+let name = "";
+let tel = "";
+let message = "";
+
+
+let username = document.getElementById("name");
+let usertel = document.getElementById("tel");
+let usermessage = document.getElementById("message");
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    tg.sendData(username,usertel,usermessage)
+});
+
