37d5e134da814642d3a523d5c97e006f3d843d0b
skorovdanil <62069661+skorovdanil@users.noreply.github.com>
2023-11-08 13:10:41 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,27 +5,31 @@ window.onload = function() {
       if (this.value.length === 18) {
       }
     });
-
+  
     var textareaElements = document.querySelectorAll("#message, #tel, #name"); // Выборка элементов по их ID
-
-    textareaElements.forEach(function(textarea) {
-      textarea.addEventListener("focus", function() {
-        document.body.style.height = "120vh";
+  
+    var isMobile = /iPhone|iPad|iPod|Android|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent);
+  
+    if (isMobile) {
+      textareaElements.forEach(function(textarea) {
+        textarea.addEventListener("focus", function() {
+          document.body.style.height = "120vh";
+        });
+        textarea.addEventListener("blur", function() {
+          document.body.style.height = "100vh";
+        });
       });
-      textarea.addEventListener("blur", function() {
-        document.body.style.height = "100vh";
+  
+      let username = document.getElementById("name");
+      let usertel = document.getElementById("tel");
+      let usermessage = document.getElementById("message");
+  
+      function sendDataToTelegram() {
+        // Отправка данных в Telegram
+      }
+  
+      Telegram.WebApp.onEvent("mainButtonClicked", function(){
+        sendDataToTelegram(username.value, usertel.value, usermessage.value);
       });
-    });
-
-
-    let username = document.getElementById("name");
-    let usertel = document.getElementById("tel");
-    let usermessage = document.getElementById("message");
-
-    function sendDataToTelegram() {
     }
-
-    Telegram.WebApp.onEvent("mainButtonClicked", function(){
-      sendDataToTelegram(username.value, usertel.value, usermessage.value);
-    });
   };
