94945ccc7196d11e18900d2d7512cddf23fb2886
skorovdanil <62069661+skorovdanil@users.noreply.github.com>
2023-11-09 10:29:24 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,17 +19,27 @@ window.onload = function() {
           document.body.style.height = "100vh";
         });
       });
-  
-      let username = document.getElementById("name");
-      let usertel = document.getElementById("tel");
-      let usermessage = document.getElementById("message");
-  
-      function sendDataToTelegram() {
+    };
+    let tg = window.Telegram.WebApp;
+    tg.expand();
+    let username = document.getElementById("name").value;
+    let usertel = document.getElementById("tel").value;
+    let usermessage = document.getElementById("message").value;
+
+    tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+    tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+    tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+    tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+    tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+    btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+      if (tg.MainButton.isVisible){ //если кнопка показана 
+        tg.MainButton.hide() //скрываем кнопку 
       }
-  
-      Telegram.WebApp.onEvent("mainButtonClicked", function(){
-        sendDataToTelegram(username.value, usertel.value, usermessage.value);
-        close();
-      });
-    }
+      else{ //иначе
+        tg.MainButton.show() //показываем 
+      }
+    });
+    Telegram.WebApp.onEvent("mainButtonClicked", function(){
+      tg.sendData(username,usertel,usermessage)
+    });
   };
