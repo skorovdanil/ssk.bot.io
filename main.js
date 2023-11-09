b0033ff05d0c888aefc13d235c975d1b98df2feb
skorovdanil <62069661+skorovdanil@users.noreply.github.com>
2023-11-09 12:00:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,40 +5,35 @@ window.onload = function() {
       if (this.value.length === 18) {
       }
     });
-  
-    var textareaElements = document.querySelectorAll("#message, #tel, #name");
-  
-    var isMobile = /iPhone|iPad|iPod|Android|Opera Mini|IEMobile|Mobile/i.test(navigator.userAgent);
-  
-    if (isMobile) {
-      textareaElements.forEach(function(textarea) {
-        textarea.addEventListener("focus", function() {
-          document.body.style.height = "120vh";
-        });
-        textarea.addEventListener("blur", function() {
-          document.body.style.height = "100vh";
-        });
-      });
-    };
+
+
     let tg = window.Telegram.WebApp;
     tg.expand();
+
+
     tg.MainButton.text = "Отправить"; //изменяем текст кнопки 
     tg.MainButton.setText ("Отправить"); //изменяем текст кнопки 
     tg.MainButton.textColor = "#FFFFFF"; //изменяем цвет текста кнопки
     tg.MainButton.color = "#A057EF"; //изменяем цвет бэкграунда кнопки
-    btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-      if (tg.MainButton.isVisible){ //если кнопка показана 
-        tg.MainButton.hide() //скрываем кнопку 
+
+
+    let form = document.getElementById('myForm');
+    let username = document.getElementById("name").value;
+    let usertel = document.getElementById("tel").value;
+    let usermessage = document.getElementById("message").value;
+
+
+    form.addEventListener('input', function(){ 
+      if (username.length > 5 && usertel > 10){ 
+        tg.MainButton.show()
       }
-      else{ //иначе
-        tg.MainButton.show() //показываем 
+      else{
+        tg.MainButton.hide() 
       }
     });
+
+
     Telegram.WebApp.onEvent("mainButtonClicked", function(){
-      let username = document.getElementById("name").value;
-      let usertel = document.getElementById("tel").value;
-      let usermessage = document.getElementById("message").value;
-  
       let data = {
         name: username,
         tel: usertel,
