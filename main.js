34c124871777d1b3178b26c77c36108860384623
skorovdanil <62069661+skorovdanil@users.noreply.github.com>
2023-11-10 11:55:00 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ window.onload = function() {
       }
     });
 
+    
+
+    function setThemeClass() {
+      document.documentElement.className = Telegram.WebApp.colorScheme;
+  }
+
+  Telegram.WebApp.onEvent('themeChanged', setThemeClass);
+  setThemeClass();
 
     let tg = window.Telegram.WebApp;
+
     tg.ready();
     tg.expand();
 
